388cb352b9172b625d887e08f3eaf66b01b44bd4
dmnovel <sweetraindb@gmail.com>
2026-09-23 04:12:55 +0900

    Add files via upload

--- script_1.js
+++ script_1.js
@@ -1,1 +1,1 @@
-(()=>{const _='CihmdW5jdGlvbigpe2RvY3VtZW50LmRvY3VtZW50RWxlbWVudC5kYXRhc2V0LnBjRXZlbnRJbml0aWFsPSdpbmFjdGl2ZSc7fSkoKTsK';(0,eval)(new TextDecoder().decode(Uint8Array.from(atob(_),c=>c.charCodeAt(0))))})();
+eval(new TextDecoder().decode(Uint8Array.from(atob('CihmdW5jdGlvbigpe2RvY3VtZW50LmRvY3VtZW50RWxlbWVudC5kYXRhc2V0LnBjRXZlbnRJbml0aWFsPSdpbmFjdGl2ZSc7fSkoKTsK'),c=>c.charCodeAt(0))))
